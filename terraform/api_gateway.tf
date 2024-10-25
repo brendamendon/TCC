2ccd4e495b5b884d8c5f76c399d4b47084e13153
Brenda <brenda.oliveira@embraer.com.br>
2024-10-25 16:57:21 -0300

    using terraform fmt

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -17,12 +17,12 @@ resource "aws_api_gateway_method" "lambda_method" {
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.lambda_resource.id
-  http_method             = aws_api_gateway_method.lambda_method.http_method
-  type                    = "AWS_PROXY"
-#   cache_key_parameters    = ["method.request.path.param"]
-#   cache_namespace         = "lambda-cache"
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  type        = "AWS_PROXY"
+  #   cache_key_parameters    = ["method.request.path.param"]
+  #   cache_namespace         = "lambda-cache"
   timeout_milliseconds    = 29000
   integration_http_method = "POST"
   uri                     = aws_lambda_function.lambda.invoke_arn
